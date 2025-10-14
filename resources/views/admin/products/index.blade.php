69fe1bb74bab9895be97e11d4c8eedae1590d72d
ophari <ophari10@gmail.com>
2025-10-14 13:43:25 +0700

    fix delete modal and edit form

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -137,32 +137,5 @@
 @endsection
 
 @push('scripts')
-<script>
-document.addEventListener('DOMContentLoaded', function () {
-    // SweetAlert for delete confirmation
-    const deleteButtons = document.querySelectorAll('.delete-btn');
-    deleteButtons.forEach(button => {
-        button.addEventListener('click', function (e) {
-            e.preventDefault();
-            const form = this.closest('form');
-            Swal.fire({
-                title: 'Are you sure?',
-                text: "You won't be able to revert this!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#d33',
-                cancelButtonColor: '#6c757d',
-                confirmButtonText: 'Yes, delete it!'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    form.submit();
-                }
-            });
-        });
-    });
-
-    // Remove jQuery dependency for this script
-    // The script now uses vanilla JavaScript
-});
-</script>
+@include('admin.products.partials.delete')
 @endpush
