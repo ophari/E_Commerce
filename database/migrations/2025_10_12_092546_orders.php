285275920bc206f303f3f33c1fc8186ae3b084a3
ophari <ophari10@gmail.com>
2025-12-11 00:38:21 +0700

    fix

--- database/migrations/2025_10_12_092546_orders.php
+++ database/migrations/2025_10_12_092546_orders.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
              $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->decimal('total_price', 10, 2)->default(0);
+            $table->decimal('total_price', 15, 2)->default(0);
             $table->enum('status', ['unpaid', 'paid', 'pending', 'processing', 'shipped', 'delivered', 'cancelled'])->default('pending');
             $table->text('shipping_address')->nullable();
             $table->timestamps();
