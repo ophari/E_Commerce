2634aafdb7a916df520940fa7c233b875e54249b
ophari <ophari10@gmail.com>
2025-12-10 22:37:12 +0700

    fix index

--- public/index.php
+++ public/index.php
@@ -5,16 +5,22 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+// Detect environment (hosting / local)
+$isHosting = str_contains(__DIR__, 'public_html');
+
+// Path base folder
+$basePath = $isHosting ? __DIR__ . '/../watchstire' : __DIR__ . '/..';
+
+// Maintenance mode...
+if (file_exists($maintenance = $basePath . '/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+// Autoload (composer)
+require $basePath . '/vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once $basePath . '/bootstrap/app.php';
 
+// Handle request
 $app->handleRequest(Request::capture());
